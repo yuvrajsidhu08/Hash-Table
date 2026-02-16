f57a3726a2c85b2178d951319bfcb1b0a1bf0a23
yuvrajsidhu08 <454009@bsd48.org>
2026-02-15 23:33:14 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -102,3 +102,56 @@ char** loadNames(const char* file, int& count) {
   }
   return names;
 }
+
+int main() {
+  srand(time(0));
+
+  int size = 100;
+  Node** table = new Node*[size];
+  for (int i = 0; i < size; i++) table[i] = 0;
+
+  int id = 1;
+
+  int fCount, lCount;
+  char** first = loadNames("first.txt", fCount);
+  char** last = loadNames("last.txt", lCount);
+
+  char cmd[20];
+
+  while (true) {
+    cout << "\nADD PRINT DELETE GENERATE QUIT\n";
+    cin >> cmd;
+
+    if (!strcmp(cmd, "ADD")) {
+      char f[50], l[50];
+      float g;
+      cin >> f >> l >> g;
+      addStudent(table, size, new Student(f, l, id++, g));
+    }
+
+    else if (!strcmp(cmd, "PRINT")) print(table, size);
+
+    else if (!strcmp(cmd, "DELETE")) {
+      int d; cin >> d;
+      removeStudent(table, size, d);
+    }
+
+    else if (!strcmp(cmd, "GENERATE")) {
+      int n; cin >> n;
+      for (int i = 0; i < n; i++) {
+        addStudent(
+          table,
+          size,
+          new Student(
+            first[rand() % fCount],
+            last[rand() % lCount],
+            id++,
+            (rand() % 401) / 100.0f
+          )
+        );
+      }
+    }
+
+    else if (!strcmp(cmd, "QUIT")) break;
+  }
+}
