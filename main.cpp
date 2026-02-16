27461f07c5501f13e172a91ddfba30486060cb7b
yuvrajsidhu08 <454009@bsd48.org>
2026-02-15 20:56:44 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -29,3 +29,41 @@ void rehash(Node**& table, int& size) {
       newTable[index] = temp;
     }
   }
+
+  delete[] table;
+  table = newTable;
+}
+
+void addStudent(Node**& table, int& size, Student* s) {
+  int index = hashID(s->getID(), size);
+
+  Node* n = new Node(s);
+  n->setNext(table[index]);
+  table[index] = n;
+
+   int count = 0;
+  Node* current = table[index];
+  while (current) {
+    count++;
+    current = current->getNext();
+  }
+
+  if (count > 3)
+    rehash(table, size);
+}
+
+void print(Node** table, int size) {
+  for (int i = 0; i < size; i++) {
+    Node* current = table[i];
+    while (current) {
+      Student* s = current->getStudent();
+      cout << s->getFirst() << " "
+           << s->getLast()
+           << " | ID: " << s->getID()
+           << " | GPA: "
+           << fixed << setprecision(2)
+           << s->getGPA() << endl;
+      current = current->getNext();
+    }
+  }
+}
