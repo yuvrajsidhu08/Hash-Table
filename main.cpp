4d9ca569b607bc734162bcde9d8fa409fded3be3
yuvrajsidhu08 <454009@bsd48.org>
2026-02-15 19:57:32 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -11,3 +11,21 @@ using namespace std;
 int hashID(int id, int size) {
   return id % size;
 }
+
+void rehash(Node**& table, int& size) {
+  int oldSize = size;
+  size *= 2;
+
+  Node** newTable = new Node*[size];
+  for (int i = 0; i < size; i++) newTable[i] = 0;
+
+  for (int i = 0; i < oldSize; i++) {
+    while (table[i]) {
+      Node* temp = table[i];
+      table[i] = table[i]->getNext();
+
+      int index = hashID(temp->getStudent()->getID(), size);
+      temp->setNext(newTable[index]);
+      newTable[index] = temp;
+    }
+  }
