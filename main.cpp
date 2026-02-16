d56e801cd60d51c26f8685385800cdb115226769
yuvrajsidhu08 <454009@bsd48.org>
2026-02-15 23:45:44 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -8,23 +8,29 @@
 
 using namespace std;
 
+// Simple hash function using ID
 int hashID(int id, int size) {
   return id % size;
 }
 
+// Double table size and rehash all students
 void rehash(Node**& table, int& size) {
   int oldSize = size;
   size *= 2;
 
+  // Create new larger table
   Node** newTable = new Node*[size];
-  for (int i = 0; i < size; i++) newTable[i] = 0;
+  for (int i = 0; i < size; i++)
+    newTable[i] = 0;
 
+  // Move every student into new table
   for (int i = 0; i < oldSize; i++) {
     while (table[i]) {
       Node* temp = table[i];
       table[i] = table[i]->getNext();
 
       int index = hashID(temp->getStudent()->getID(), size);
+
       temp->setNext(newTable[index]);
       newTable[index] = temp;
     }
@@ -34,13 +40,16 @@ void rehash(Node**& table, int& size) {
   table = newTable;
 }
 
+// Add a student into the hash table
 void addStudent(Node**& table, int& size, Student* s) {
   int index = hashID(s->getID(), size);
 
+  // Insert at front of linked list
   Node* n = new Node(s);
   n->setNext(table[index]);
   table[index] = n;
 
+  // Count nodes in this chain
   int count = 0;
   Node* current = table[index];
   while (current) {
@@ -48,10 +57,12 @@ void addStudent(Node**& table, int& size, Student* s) {
     current = current->getNext();
   }
 
+  // If more than 3 collisions, resize table
   if (count > 3)
     rehash(table, size);
 }
 
+// Print all students in table
 void print(Node** table, int size) {
   for (int i = 0; i < size; i++) {
     Node* current = table[i];
@@ -68,6 +79,7 @@ void print(Node** table, int size) {
   }
 }
 
+// Remove a student by ID
 void removeStudent(Node** table, int size, int id) {
   int index = hashID(id, size);
 
@@ -76,8 +88,11 @@ void removeStudent(Node** table, int size, int id) {
 
   while (curr) {
     if (curr->getStudent()->getID() == id) {
-      if (!prev) table[index] = curr->getNext();
-      else prev->setNext(curr->getNext());
+      if (!prev)
+        table[index] = curr->getNext();
+      else
+        prev->setNext(curr->getNext());
+
       delete curr;
       return;
     }
@@ -86,20 +101,28 @@ void removeStudent(Node** table, int size, int id) {
   }
 }
 
+// Load names from file into dynamic array
 char** loadNames(const char* file, int& count) {
   ifstream f(file);
   char temp[100];
   count = 0;
 
-  while (f.getline(temp, 100)) count++;
-  f.clear(); f.seekg(0);
+  // Count lines first
+  while (f.getline(temp, 100))
+    count++;
 
+  f.clear();
+  f.seekg(0);
+
+  // Allocate array of C-strings
   char** names = new char*[count];
+
   for (int i = 0; i < count; i++) {
     f.getline(temp, 100);
     names[i] = new char[strlen(temp) + 1];
     strcpy(names[i], temp);
   }
+
   return names;
 }
 
@@ -108,36 +131,52 @@ int main() {
 
   int size = 100;
   Node** table = new Node*[size];
-  for (int i = 0; i < size; i++) table[i] = 0;
 
-  int id = 1;
+  // Initialize table to null
+  for (int i = 0; i < size; i++)
+    table[i] = 0;
 
+  int id = 1; // Auto-increment student ID
+
+  // Load name files
   int fCount, lCount;
   char** first = loadNames("first.txt", fCount);
   char** last = loadNames("last.txt", lCount);
 
   char cmd[20];
 
+  // Main command loop
   while (true) {
     cout << "\nADD PRINT DELETE GENERATE QUIT\n";
     cin >> cmd;
 
+    // Manually add student
     if (!strcmp(cmd, "ADD")) {
       char f[50], l[50];
       float g;
+
       cin >> f >> l >> g;
-      addStudent(table, size, new Student(f, l, id++, g));
+
+      addStudent(table, size,
+                 new Student(f, l, id++, g));
     }
 
-    else if (!strcmp(cmd, "PRINT")) print(table, size);
+    // Print all students
+    else if (!strcmp(cmd, "PRINT"))
+      print(table, size);
 
+    // Delete by ID
     else if (!strcmp(cmd, "DELETE")) {
-      int d; cin >> d;
+      int d;
+      cin >> d;
       removeStudent(table, size, d);
     }
 
+    // Generate random students
     else if (!strcmp(cmd, "GENERATE")) {
-      int n; cin >> n;
+      int n;
+      cin >> n;
+
       for (int i = 0; i < n; i++) {
         addStudent(
           table,
@@ -152,6 +191,8 @@ int main() {
       }
     }
 
-    else if (!strcmp(cmd, "QUIT")) break;
+    // Exit program
+    else if (!strcmp(cmd, "QUIT"))
+      break;
   }
 }
